10d5f2ebacabd08d877cb90b9139447ec23652f4
jonathan <jfonsecac2@ucentral.edu.co>
2022-02-16 19:32:08 -0500

    commit inicial

---
+++ src/test/java/co/com/choucair/certification/proyectobase/runners/runnerTags.java
@@ -0,0 +1,15 @@
+package co.com.choucair.certification.proyectobase.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features",
+        tags = "@LoginSuccessful",
+        glue = "stepDefinitions"
+)
+public class runnerTags {
+}
